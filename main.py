47d751c35616f6894057509f859ff0982ea11edc
PhilemonVanDussen <vandussenph709@northwested.org>
2024-09-25 14:30:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+# PJ VanDussen
+# 9/25/24
+# Simple Math
+
+#Practice 1
+# num1 = 2
+# num2 = 3
+
+# sum = num1 + num2 
+# difference = num2 - num1
+# product = num1 * num2
+# quotient = num2 / num1
+
+# print(f'The sum of {num1} and {num2} is {sum}')
+# print(f'The difference of {num2} and {num1} is {difference}')
+# print(f'The product of {num1} and {num2} is {product}')
+# print(f'The quotient of {num2} and {num1} is {quotient}')
+
+#Practice 2
+length = float(input('Enter the length of your rectangular room in feet \n'))
+width = float(input('Enter the width of your rectangular room in feet \n'))
+
+area = length * width 
+
+print(f'The length ({length} ft) and the width ({width} ft) of your room, times them together and the area is {area:.1f} sq. ft.')
+
+
 
